003d121d94ffe58de4e5b356864b2982492868bd
Damián Cumaco <drecm0nd@gmail.com>
2026-05-29 15:54:17 -0500

    Ejercicio de modelos con Django

---
+++ practica/proyecto_practica/mi_ejercicio_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MiEjercicioAppConfig(AppConfig):
+    name = 'mi_ejercicio_app'
---
+++ practica/proyecto_practica/mi_ejercicio_app/models.py
@@ -0,0 +1,61 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100)
+    precio = models.DecimalField(max_digits=6, decimal_places=2)
+    disponible = models.BooleanField(default=True)
+    descripcion = models.CharField(max_length=200)
+    stock = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f'Codigo: {self.codigo} Descripción: {self.descripcion}'
+    
+class Automovil (models.Model):
+    matricula = models.CharField(max_length=6, unique=True)
+    modelo = models.CharField(max_length=100)
+    marca = models.CharField(max_length=100)
+    al_dia_impuestos = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f'Matricula {self.matricula} Marca {self.marca}'
+    
+class Futbolista (models.Model):
+    nombre = models.CharField(max_length=100)
+    nacionalidad = models.CharField(max_length=100)
+    fecha_nacimiento = models.DateField()
+    peso = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f'Nombre: {self.nombre}'
+    
+class Instructor (models.Model):
+    documento = models.CharField(max_length=30, unique=True)
+    nombre = models.CharField(max_length=100)
+    apellido1 = models.CharField(max_length=100)
+    apellido2 = models.CharField(max_length=100, null=True)
+    genero = models.CharField(max_length=100)
+    ciudad_residencia = models.CharField(max_length=100)
+    direccion = models.CharField(max_length=150)
+    telefono = models.CharField(max_length=20, unique=True)
+    correo = models.EmailField()
+    profesion = models.CharField(max_length=150)
+    especialidad = models.CharField(max_length=150)
+    fecha_nacimiento = models.DateField()
+    fecha_ingreso = models.DateField()
+    salario_basico = models.DecimalField(max_digits=10, decimal_places=2)
+    programado = models.BooleanField(default=True)
+
+def __str__(self):
+    return f'Num document: {self.documento} | Nombre: {self.nombre}'
+
+class Futbolista(models.Model):
+    nombre = models.CharField(max_length=100)
+    nacionalidad = models.CharField(max_length=100)
+    fecha_nacimiento = models.DateField()
+    peso = models.DecimalField(max_digits=10, decimal_places=2)
+
+def __str__(self):
+    return f'Nombre {self.nombre}'
